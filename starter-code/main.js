f30c786292a9d566b032622f2af07d4ae7393d43
Samantha Ki <thesamanthaki@gmail.com>
2016-10-21 18:08:36 +1100

    created cards using JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,7 +5,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("Sorry, try again.")
 }
 	else if (cardOne === cardTwo) {
@@ -16,4 +16,16 @@ if (cardTwo === cardFour) {
 	}
 	else {
 		alert("Sorry, try again.")
+	} */
+
+var gameBoard = document.getElementById('game-board');
+
+var cards = 4;
+
+var createCards = function() {
+	for (i = 0; i < cards; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
 	}
+};
